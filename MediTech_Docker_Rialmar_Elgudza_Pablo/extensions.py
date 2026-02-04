efea6b6c15661932dc084e550502d91a04e2a1c1
pazofral03 <pazofral03@larioja.edu.es>
2026-02-04 19:15:16 +0100

    Delete MediTech_Docker_Rialmar_Elgudza_Pablo directory

--- MediTech_Docker_Rialmar_Elgudza_Pablo/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.login_message = "Por favor inicia sesión para acceder a esta página."
-login_manager.login_message_category = "info"
